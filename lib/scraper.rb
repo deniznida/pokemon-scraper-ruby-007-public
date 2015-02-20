29972929bb707d766786e8234c9f59eddf75477d
deniznida <denizunat@hotmail.com>
2015-02-20 16:56:32 -0500

    done

---
+++ lib/scraper.rb
@@ -0,0 +1,17 @@
+class Scraper
+
+  def initialize(db)
+    @db = db
+    #@html = File.open("pokemon_index.html")
+  end
+
+  def scrape
+    doc = Nokogiri::HTML(open("pokemon_index.html"))
+
+    doc.search("span.infocard-tall").collect do |pokemon|
+      name = pokemon.search("a.ent-name").text
+      type = pokemon.search("small.aside a").text
+      Pokemon.save(name, type, @db)
+    end 
+  end
+end
